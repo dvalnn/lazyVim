b1304e9e787c76c187f95a9f4ed165813a714b65
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-11-11 16:18:18 +0000

    disabled neo-tree on startup

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "Neotree right",
-})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "Neotree toggle",
-})
-
 require("config.lazy")
