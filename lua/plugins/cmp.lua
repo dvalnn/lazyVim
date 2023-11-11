3a359314896c883861577a48bcf742f1eb8f6285
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-11-11 17:33:05 +0000

    highlight group changes
    fixed some highlight groups that were not properly set to be transparent or have the correct colour.

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      window = {
+        documentation = {
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:none",
+        },
+      },
+    },
+  },
+}
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -5,8 +5,6 @@ return {
       require("onedark").setup({
         style = "darker",
         transparent = true,
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
       })
     end,
   },
@@ -19,6 +17,10 @@ return {
         require("notify").setup({
           background_colour = "#000000",
         })
+        vim.api.nvim_set_hl(0, "Float", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
       end,
     },
   },
